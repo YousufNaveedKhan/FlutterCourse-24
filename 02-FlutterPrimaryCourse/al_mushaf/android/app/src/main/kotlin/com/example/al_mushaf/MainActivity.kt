85f836477577117f802244a14d5f5e5dda2c0ee1
YousufNaveedKhan <yk033127@gmail.com>
2025-12-07 19:44:29 +0500

    DartCourse-2024

--- 02-FlutterPrimaryCourse/al_mushaf/android/app/src/main/kotlin/com/example/al_mushaf/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.al_mushaf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 02-FlutterPrimaryCourse/page_navigator_app/android/app/src/main/kotlin/com/example/page_navigator_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.page_navigator_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
